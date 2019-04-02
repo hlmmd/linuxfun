badc6780a31c75d942d074fd4a91b3b53b96d545
hlmmd <hlmmd1995@gmail.com>
2019-04-02 19:06:38 +0800

    add deamon

---
+++ include/common.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <time.h>
+#include <signal.h>
+
+#define mff_RETURN_OK 1
+#define mff_RETURN_ERROR -1
+
+
+int mff_daemonize() ;
--- src/main.c
+++ src/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include "SocketfunConfig.h"
+#include "common.h"
+
 int main(int argc, char *argv[])
 {
-    printf("%d.%d\n",Socketfun_VERSION_MAJOR,Socketfun_VERSION_MINOR);
+    printf("%d.%d\n", Socketfun_VERSION_MAJOR, Socketfun_VERSION_MINOR);
     if (argc < 2)
     {
 
         //  std::cout << "Usage: " << argv[0] << " number" << std::endl;
-        return 1;
+        //    return 1;
     }
+
+    mff_daemonize();
+
+    //execl(argv[1],argv[1],NULL);
+
+    // printf("%d.%d\n", Socketfun_VERSION_MAJOR, Socketfun_VERSION_MINOR);
 
     return 0;
 }
