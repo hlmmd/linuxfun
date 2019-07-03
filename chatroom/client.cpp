bcc001180385d106948507b9260b246d7939daaf
hlmmd <hlmmd1995@gmail.com>
2019-07-03 20:10:39 +0800

    update chatroom

--- chatroom/client.cpp
+++ chatroom/client.cpp
@@ -9,15 +9,123 @@
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <errno.h>
+// #define BUFFER_SIZE 64
 
-#define BUFFER_SIZE 64
+// int main( int argc,char ** argv)
+// {
+//     if(argc<=2)
+//     {
+//         printf("usage: ./%s ip_address port \n",basename(argv[0]));
+//         return 1;
+//     }
+//     return 0;
+// }
 
-int main( int argc,char ** argv)
-{
-    if(argc<=2)
-    {
-        printf("usage: ./%s ip_address port \n",basename(argv[0]));
-        return 1;
-    }
-    return 0;
+
+
+
+#define MAXLENGTH 64  
+int main(int argc,char **argv)
+{  
+	int sockfd ,len;
+	struct sockaddr_in server_addr;
+	char buffer[MAXLENGTH+1];
+	fd_set rfds;
+	struct timeval tv;
+	int ret,maxfd =-1,flags;
+
+	if(argc!=3)
+	{
+		printf("use: ./server IP port\n");
+		exit(-1);
+	}
+	
+	//ŽŽœšsocket
+	if((sockfd = socket (AF_INET,SOCK_STREAM,0))<0)
+	{
+		perror("socket");
+		exit(-1);
+	}
+	
+	//³õÊŒ»¯·þÎñÆ÷µØÖ·ºÍ¶Ë¿Ú
+	bzero(&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(atoi(argv[2]));
+	if(inet_aton(argv[1],(struct in_addr *)&server_addr.sin_addr.s_addr) == 0){  
+        perror(argv[1]);  
+        exit(errno);  
+    }  
+	
+	if ((flags=fcntl(sockfd, F_GETFL)) < 0)
+		return -1;
+	if (fcntl(sockfd, F_SETFL, flags|O_NONBLOCK) < 0)
+		return -1;
+	
+	//ÔÚÒì²œÄ£ÊœÏÂ£¬ÈÎºÎÇé¿öÏÂconnectµÄ·µ»ØÖµ¶ŒÊÇ-1£¬ŽËÊ±ÐèÍš¹ýerrnoÅÐ¶ÏÁ¬œÓÊÇ·ñ³É¹Š
+	connect(sockfd,(struct sockaddr*)&server_addr,sizeof(server_addr));
+	if(errno != EINPROGRESS)    
+	{
+        perror("connect");  
+        exit(errno); 
+    }  
+	printf("connected server !\n");
+	
+	while(1)
+	{
+		//Çå¿ÕŒ¯ºÏ
+		FD_ZERO(&rfds);
+		//°Ñ±ê×ŒÊäÈëµÄŸä±ú0ŒÓÈëµœŒ¯ºÏ
+		FD_SET(0,&rfds);
+		maxfd = 0;
+		//œ«µ±Ç°socketŸä±úŒÓÈëŒ¯ºÏ
+		FD_SET(sockfd,&rfds);
+		if(sockfd>maxfd)
+			maxfd = sockfd;
+		//ÉèÖÃ×îŽóµÈŽýÊ±Œä
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
+		//select
+		ret = select(maxfd+1,&rfds,NULL,NULL,&tv);
+		if(ret ==-1){
+			printf("select error\n");
+			break;
+		}
+		else if(ret ==0)
+			continue;
+		else{
+			if(FD_ISSET(sockfd,&rfds)){//socket¿É¶Á
+				bzero(buffer,MAXLENGTH+1);
+				len = recv(sockfd,buffer,MAXLENGTH,0);
+				if(len>0)
+				printf("recv from server: %s\n",buffer);
+				else if(len<0)
+						printf("receive failed!\n");
+				else{
+					printf("server down\n");
+					break;
+				}
+			}
+			if(FD_ISSET(0,&rfds)){//ÓÐÊäÈë
+				bzero(buffer,MAXLENGTH+1);
+				fgets(buffer,MAXLENGTH,stdin);
+				buffer[strlen(buffer)-1]='\0' ;
+				
+				len = send(sockfd,buffer,strlen(buffer),0);
+				if(len<0){
+					printf("send error\n");
+					break;
+					}
+				else if(len!=0)
+					printf("send msg: %s \n",buffer);
+			}
+
+		}
+		
+
+	}
+
+	close(sockfd);
+
+    return 0;  
 }
--- chatroom/server.cpp
+++ chatroom/server.cpp
@@ -11,27 +11,25 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-#define USER_LIMIT 5 /* 最大用户数  */
-#define BUFFER_SIZE 64  //  缓冲区大小
-#define FD_LIMIT 65535  // fd限制
+#define USER_LIMIT 5   /* 最大用户数  */
+#define BUFFER_SIZE 64 //  缓冲区大小
+#define FD_LIMIT 65535 // fd限制
 
-
-struct client_data{
+struct client_data
+{
     struct sockaddr_in client_address;
     int sockfd;
-    char * write_buffer;
+    char *write_buffer;
     char buffer[BUFFER_SIZE];
 };
 
 int setnonblocking(int fd)
 {
-    int old_opt = fcntl(fd,F_GETFL);
-    int new_opt = old_opt|O_NONBLOCK;
-    fcntl(fd,F_SETFL,new_opt);
+    int old_opt = fcntl(fd, F_GETFL);
+    int new_opt = old_opt | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_opt);
     return old_opt;
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -42,65 +40,107 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    const char* ip = argv[1];
+    const char *ip = argv[1];
     int port = atoi(argv[2]);
-
 
     //根据IP、port设置sockaddr_in结构体
     struct sockaddr_in address;
-    memset(&address,0,sizeof(address));
-    address.sin_family=AF_INET;
-    inet_pton(AF_INET,ip,&address.sin_addr);
-    address.sin_port = htons(port);//主机序转网络序
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port); //主机序转网络序
 
-    int ret ;
-    int listenfd = socket(AF_INET,SOCK_STREAM,0);
-    assert(listenfd>=0);
+    int ret;
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
 
     int opt = 1;
-	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    ret = bind(listenfd,(struct sockaddr*) &address,sizeof(address));
-    assert(ret!=-1);
+    ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != -1);
 
-    printf("%d\n",ret);
+    setnonblocking(listenfd);
 
-    ret = listen(listenfd,5);
-    assert(ret!=-1);
+    printf("%d\n", ret);
 
-    client_data *users = (client_data*) malloc(sizeof(client_data)*USER_LIMIT);
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
 
-    while(1){
+    client_data *users = (client_data *)malloc(sizeof(client_data) * USER_LIMIT);
+
+    int current_users = 0;
+
+    while (1)
+    {
         struct sockaddr_in client_addr;
         socklen_t client_addr_length = sizeof(client_addr);
         int clientfd;
-		if ((clientfd = accept(listenfd, (struct sockaddr *)&client_addr, &client_addr_length)) >= 0)
+        if ((clientfd = accept(listenfd, (struct sockaddr *)&client_addr, &client_addr_length)) >= 0)
         {
-			printf("Connected from %s\n", inet_ntoa(client_addr.sin_addr));
-            
+            printf("Connected from %s\n", inet_ntoa(client_addr.sin_addr));
+            users[current_users].sockfd = clientfd;
+            current_users++;
+        }
+        else
+        {
 
+            while (1)
+            {
+                printf("aa\n");
+                sleep(1);
 
+                fd_set rfds;
+                int maxfd;
+                //清空集合
+                FD_ZERO(&rfds);
+                maxfd = 0;
+                //将当前连接句柄加入到集合中
+                for (int i = 0; i < current_users; i++)
+                {
+                    FD_SET(users[current_users].sockfd, &rfds);
+                    maxfd = maxfd > users[current_users].sockfd ? maxfd : users[current_users].sockfd;
+                }
+                struct timeval tv;
+                tv.tv_sec = 1;
+                tv.tv_usec = 0;
+                ret = select(maxfd + 1, &rfds, NULL, NULL, &tv);
+                if (ret == -1)
+                {
+                    printf("select error\n");
+                    break;
+                }
+                else if (ret == 0)
+                    break;
+                else
+                {
+                    for (int i = 0; i < current_users; i++)
+                    {
+                        if (FD_ISSET(users[current_users].sockfd, &rfds))
+                        { //socket有消息
+                            bzero(users[current_users].buffer, BUFFER_SIZE);
+                            int len = recv(users[current_users].sockfd, users[current_users].buffer, BUFFER_SIZE, 0);
 
+                            if (len > 0)
+                            {
+                                printf("recv from client[%d]: %s\n", users[current_users].sockfd, users[current_users].buffer);
+                            }
+                            else if (len == 0)
+                            {
+                                printf("client %d disconnected!\n", users[current_users].sockfd);
+                                break;
+                            }
+                        }
+                    }
+                }
+            } //内while
+        }
 
-
-        };
-
-
-
-
-		// len = send(clientfd,"hello",5,0);
-
-		// len = recv(clientfd,buffer,MAXLENGTH,0);
-		// printf("%s\n",buffer);
-		break;
-		
-
-
-
+        //   printf("aaaa");
     }
 
     free(users);
     close(listenfd);
-    
+
     return 0;
 }
